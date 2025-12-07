bee5fc5b745614274d27e510cbcb02b31b927801
Ayush Kumar <158518323+AyushKumarDEVs@users.noreply.github.com>
2025-12-07 22:42:04 +0530

    Change API endpoint to use relative path

--- frontend/src/components/ProductGrid.jsx
+++ frontend/src/components/ProductGrid.jsx
@@ -22,8 +22,8 @@ export default function ProductGrid() {
        
 
         const endpoint = islatest
-          ? `${"https://perfume-shop-five.vercel.app"}/product-lists/latest`
-          : `${"https://perfume-shop-five.vercel.app"}/product-lists/featured`;
+          ? `${"/api"}/product-lists/latest`
+          : `${"/api"}/product-lists/featured`;
 
         const res = await axios.get(endpoint);
         setListData(res.data);
