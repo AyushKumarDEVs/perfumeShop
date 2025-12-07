347a73d703318237b19c62be8cd605613bd9cfc9
Ayush Kumar <158518323+AyushKumarDEVs@users.noreply.github.com>
2025-12-07 22:41:07 +0530

    proxy set

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000, // default port for Vite
+    proxy: {
+      '/api': {
+        target: 'https://perfumeshop-backend.onrender.com',
+      }}},
   plugins: [react()],
 })
